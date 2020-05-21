12fae02fcf3a78413a0658bb15666683a4038e7b
raemasae <masae1263@gmail.com>
2020-05-20 20:09:25 -1000

    update css file path

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <title>WeatherScraperDemo</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="weatherScraper.css">
+    <link rel="stylesheet" href="style.css">
   </head>
 
   <body>
